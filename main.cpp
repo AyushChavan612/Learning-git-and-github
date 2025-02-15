3dc4bc9117f03b6a6785013526545283bac73794
AyushChavan612 <chavanayushp@gmail.com>
2025-02-15 15:05:32 +0530

    c++2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
+using namespace std;
+
 int main() {
     std::cout << "HELLO WORLD" << std::endl;
+    for(int i =  1; i <= 10; ++i) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    for(int i = 10; i >= 0; --i) {
+        cout << i << " ";
+    }
     return 0;
 }
