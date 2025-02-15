7fb38bc7692b8dab0e4c7b104df23a1011e3a398
AyushChavan612 <chavanayushp@gmail.com>
2025-02-15 15:43:26 +0530

    old code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
 #include<iostream>
-using namespace std;
-
 int main() {
     std::cout << "HELLO WORLD" << std::endl;
-    for(int i =  1; i <= 10; ++i) {
-        cout << i << " ";
-    }
-
-    cout << endl;
     return 0;
 }
